0af00d11d31d20ff4fbbeb6317bd7fc164768fbb
dannychan0510 <dannychan0510>
2015-06-29 23:29:20 +0100

    updated analysis.R

--- analysis.R
+++ analysis.R
@@ -1,7 +1,12 @@
 setwd("~/Documents/GitHub/nba-draft-analysis")
 temp <- list.files("Data", full.names = TRUE)
-dB <- do.call(rbind, lapply(temp, function(x) read.csv(x , header = TRUE, skip = 2)))
+dB <- do.call(rbind, lapply(temp, function(x) read.csv(x , header = TRUE, skip = 2, stringsAsFactors=FALSE)))
+
+dB$Rk <- as.numeric (dB$Rk)
+
 tail(dB)
 temp
 unique(dB$Rk)
 str(dB)
+
+colCl <- c("numeric", "numeric", "character", "numeric", "numeric", "character", "character", "numeric", "character", "character",  "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric")
