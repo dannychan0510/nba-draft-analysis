619b873ab532016cae82bcfa86749c50e03c4dc5
dannychan0510 <dannychan0510>
2015-07-01 08:53:37 +0100

    updated analysis.R

--- analysis.R
+++ analysis.R
@@ -1,12 +1,29 @@
 setwd("~/Documents/GitHub/nba-draft-analysis")
 temp <- list.files("Data", full.names = TRUE)
-dB <- do.call(rbind, lapply(temp, function(x) read.csv(x , header = TRUE, skip = 2, stringsAsFactors=FALSE)))
+dB <- do.call(rbind, lapply(temp, function(x) read.csv(x , header = TRUE, skip = 2, stringsAsFactors = FALSE)))
 
-dB$Rk <- as.numeric (dB$Rk)
+names(dB)[names(dB) == "FG."] <- "FG"
+names(dB)[names(dB) == "X2P."] <- "X2P"
+names(dB)[names(dB) == "X3P."] <- "X3P"
+names(dB)[names(dB) == "FT."] <- "FT"
 
-tail(dB)
-temp
-unique(dB$Rk)
-str(dB)
+dB$Rk <- as.numeric(dB$Rk)
+dB$Year <- as.numeric(dB$Year)
+dB$Rd <- as.numeric(dB$Rd)
+dB$Pk <- as.numeric(dB$Pk)
+dB$Age <- as.numeric(dB$Age)
+dB$From <- as.numeric(dB$From)
+dB$To <- as.numeric(dB$To)
+dB$G <- as.numeric(dB$G)
+dB$PTS <- as.numeric(dB$PTS)
+dB$TRB <- as.numeric(dB$TRB)
+dB$AST <- as.numeric(dB$AST)
+dB$STL <- as.numeric(dB$STL)
+dB$BLK <- as.numeric(dB$BLK)
+dB$FG <- as.numeric(dB$FG)
+dB$X2P <- as.numeric(dB$X2P)
+dB$X3P <- as.numeric(dB$X3P)
+dB$FT <- as.numeric(dB$FT)
+dB$WS <- as.numeric(dB$WS)
+dB$WS.48 <- as.numeric(dB$WS.48)
 
-colCl <- c("numeric", "numeric", "character", "numeric", "numeric", "character", "character", "numeric", "character", "character",  "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric")
